c2b7a31dda6953863de8754053ec226e0680a1b3
Forester Erick <104191876+Forester04@users.noreply.github.com>
2024-05-22 07:20:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 22,
+        skills: ['JavaScript', 'React', 'Node'],
+        education: {
+            degree: 'Masters',
+            school: {
+                name: 'XYZ',
+                location: 'India'
+            }
+        }
+    };
+
+    return member;
+}
